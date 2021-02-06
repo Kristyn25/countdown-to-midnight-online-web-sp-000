b2b2a1a32aa851a2d11ddefc07ad4eb5e145859b
Kristyn Little <kristynlittle@frontier.com>
2021-02-06 03:52:44 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
-def countdown(number)
-  number = 10
-  while x > 0
-    puts "#{number} SECOND(S)!"
-    number -= 1
+def countdown(num)
+  num = 10
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
   end
   "HAPPY NEW YEAR!"
 end
