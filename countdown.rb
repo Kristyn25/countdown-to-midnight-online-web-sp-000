a49792373f7e94f3321b893620debe6725347844
Kristyn Little <kristynlittle@frontier.com>
2021-02-06 03:55:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,13 @@ def countdown(num)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep (num)
+  num = 10
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
+    sleep 5
+  end
+  "HAPPY NEW YEAR!" 
+end
